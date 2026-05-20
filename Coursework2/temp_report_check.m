04fa9a5c7f2751fa57cb7fb4bdd4bbaa89998b4e
Thomriii <henrytsjohnson@outlook.com>
2026-05-20 13:13:09 +0100

    edit github link

--- Coursework2/temp_report_check.m
+++
@@ -1,7 +0,0 @@
-load('matlab/furuta_results.mat');
-t = results.time;
-a = rad2deg(results.openLoop(:,2));
-idx = find(t >= 1.0, 1, 'first');
-fprintf('openAlphaAt1s=%.4f\n', a(idx));
-fprintf('openFinalAlpha=%.4f\n', a(end));
-fprintf('openPeakAbsAlpha=%.4f\n', max(abs(a)));
